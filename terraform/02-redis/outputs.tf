91a897722cc6463788c53d1b0b56c63ef681ce11
ayd.asraf <ayd.asraf@auto1.com>
2022-09-29 20:47:09 +0200

    Add ACM + SSL ALB

--- terraform/02-redis/outputs.tf
+++ terraform/02-redis/outputs.tf
@@ -1,3 +1,7 @@
 output "redis_endpoint" {
   value = "${aws_elasticache_cluster.redis.cache_nodes[0].address}:${aws_elasticache_cluster.redis.cache_nodes[0].port}"
 }
+
+output "test" {
+  value = aws_elasticache_cluster.redis.cache_nodes
+}
