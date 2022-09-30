948ceb26623bbbeb7fc869a0dd085659a6ac2792
ayd.asraf <ayd.asraf@auto1.com>
2022-09-30 11:02:51 +0200

    Add All App

--- terraform/02-redis/outputs.tf
+++ terraform/02-redis/outputs.tf
@@ -1,5 +1,5 @@
 output "url_shortener_redis_address" {
-  value = join(",", [for node in aws_elasticache_cluster.redis.cache_nodes :
+  value = join(",", [for node in aws_elasticache_cluster.url_shortener.cache_nodes :
     "${node.address}"
   ])
 }
