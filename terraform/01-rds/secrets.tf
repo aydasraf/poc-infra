c71ebb454ce0e6ca415600a8ab2e34cbe5d9617d
ayd.asraf <ayd.asraf@auto1.com>
2022-09-30 13:49:47 +0200

    Add All App

--- terraform/01-rds/secrets.tf
+++ terraform/01-rds/secrets.tf
@@ -1,5 +1,5 @@
 resource "aws_secretsmanager_secret" "url_shortener" {
-  name = "url_shortener_db"
+  name = "url_shortener_db_${terraform.workspace}"
 }
 resource "aws_secretsmanager_secret_version" "url_shortener" {
   secret_id = aws_secretsmanager_secret.url_shortener.id
